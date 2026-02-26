e369736df536ef1c5b509d2e890b7eb9502b55c5
djakovl <53625643+djakovl@users.noreply.github.com>
2026-02-26 04:08:39 +0300

    Simplify Status model to match DB schema

--- backend/task-service/models/status.go
+++ backend/task-service/models/status.go
@@ -1,12 +1,6 @@
 package models
 
-import "time"
-
 type Status struct {
-	ID         string    `json:"id"`
-	Name       string    `json:"name"`
-	Color      string    `json:"color"`
-	OrderIndex int       `json:"order_index"`
-	IsDefault  bool      `json:"is_default"`
-	CreatedAt  time.Time `json:"created_at"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
