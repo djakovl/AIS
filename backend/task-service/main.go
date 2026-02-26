bf468ec612d4743c3ca959b73de68efcf74cb7c0
djakovl <53625643+djakovl@users.noreply.github.com>
2026-02-26 10:54:21 +0300

    Add X-CSRF-Token to CORS allowed headers

--- backend/task-service/main.go
+++ backend/task-service/main.go
@@ -74,7 +74,7 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Content-Type", "X-User-Id", "Authorization"},
+		AllowHeaders:     []string{"Content-Type", "X-User-Id", "X-CSRF-Token", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
